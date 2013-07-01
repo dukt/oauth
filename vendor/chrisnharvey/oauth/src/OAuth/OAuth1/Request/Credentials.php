af8af958ffde4791eab5eb76c913f2241be7cd17
Benjamin David <ben@dukt.net>
2013-07-01 22:37:11 +0200

    Removed vendor

--- vendor/chrisnharvey/oauth/src/OAuth/OAuth1/Request/Credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace OAuth\OAuth1\Request;
-
-class Credentials extends \OAuth\OAuth1\Request
-{
-}
--- vendor/chrisnharvey/oauth/src/OAuth/OAuth1/Token/Access.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace OAuth\OAuth1\Token;
-
-class Access extends \OAuth\OAuth1\Token
-{
-    protected $name = 'access';
-} // End OAuth_Token_Access
