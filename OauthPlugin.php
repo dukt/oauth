65bafbaa33250b42ab4d8d41bc1adb2f2fa3a41a
Benjamin David <ben@dukt.net>
2013-07-02 04:06:53 +0200

    0.9.8

--- OauthPlugin.php
+++ OauthPlugin.php
@@ -20,7 +20,7 @@ class OauthPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.7';
+        return '0.9.8';
     }
 
     // --------------------------------------------------------------------
