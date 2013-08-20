7092bd4f7dc7a1bd25a2d0cf41f5b2c11c4b1690
Benjamin David <ben@dukt.net>
2013-08-20 05:51:31 +0200

    Added Oauth_ConnectController, Oauth_DisconnectController controllers

---
+++ controllers/Oauth_ConnectController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Craft;
+
+require(CRAFT_PLUGINS_PATH.'oauth/vendor/autoload.php');
+
+class Oauth_ConnectController extends BaseController
+{
+    protected $allowAnonymous = true;
+
+    // --------------------------------------------------------------------
+
+    public function init()
+    {
+        // request params
+
+        $providerClass = craft()->request->getParam('provider');
+        $namespace     = craft()->request->getParam('namespace');
+        $scope         = unserialize(base64_decode(craft()->request->getParam('scope')));
+
+        // userMode
+
+        $userMode = false;
+
+        if(!$namespace) {
+            $userMode = true;
+        }
+
+
+        // clean session vars
+
+        craft()->oauth->httpSessionClean();
+
+
+        // set session vars
+
+        craft()->oauth->httpSessionAdd('oauth.providerClass', $providerClass);
+        craft()->oauth->httpSessionAdd('oauth.userMode', $userMode);
+        craft()->oauth->httpSessionAdd('oauth.referer', (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : null));
+        craft()->oauth->httpSessionAdd('oauth.scope', $scope);
+
+
+        // redirect
+
+        $url = UrlHelper::getActionUrl('oauth/public/connect/', array(
+                'provider' => $providerClass,
+                'namespace' => $namespace
+            ));
+
+        $this->redirect($url);
+    }
+}
---
+++ controllers/Oauth_DisconnectController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Craft;
+
+require(CRAFT_PLUGINS_PATH.'oauth/vendor/autoload.php');
+
+class Oauth_DisconnectController extends BaseController
+{
+    protected $allowAnonymous = true;
+
+    // --------------------------------------------------------------------
+
+    public function init()
+    {
+        // request params
+
+        $params = array();
+        $params['provider'] = craft()->request->getParam('provider');
+        $params['namespace'] = craft()->request->getParam('namespace');
+
+
+        // clean session vars
+
+        craft()->oauth->httpSessionClean();
+
+        craft()->oauth->httpSessionAdd('oauth.referer', $_SERVER['HTTP_REFERER']);
+
+        // redirect
+
+        $url = UrlHelper::getActionUrl('oauth/public/disconnect/', $params);
+
+        $this->redirect($url);
+    }
+}
