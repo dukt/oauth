b343eeddb7f18a6adf21a1cf864a430fda87abb4
Benjamin David <ben@dukt.net>
2013-09-07 16:11:28 +0200

    Added migration for  renaming `oauth_tokens`.`providerClass` to `oauth_tokens`.`class`

--- OauthPlugin.php
+++ OauthPlugin.php
@@ -20,7 +20,7 @@ class OauthPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.14';
+        return '0.9.15';
     }
 
     // --------------------------------------------------------------------
