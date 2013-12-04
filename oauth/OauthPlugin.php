f06ef2819620a22fda252792941c1e391b3cdf23
Benjamin David <ben@dukt.net>
2013-12-04 20:10:48 +0100

    OAuth 0.9.40

--- oauth/OauthPlugin.php
+++ oauth/OauthPlugin.php
@@ -29,7 +29,7 @@ class OauthPlugin extends BasePlugin
      */
     function getVersion()
     {
-        return '0.9.39';
+        return '0.9.40';
     }
 
     // --------------------------------------------------------------------
