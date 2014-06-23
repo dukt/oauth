d56b23a50a0dd58fdc5310cef15227c95cd90dc0
Benjamin David <ben@dukt.net>
2014-06-24 01:07:05 +0200

    old/new tokens table migration

---
+++ oauth/migrations/m140623_130304_oauth_new_tokens_table.php
@@ -0,0 +1,36 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m140623_130304_oauth_new_tokens_table extends BaseMigration
+{
+	/**
+	 * Any migration code in here is wrapped inside of a transaction.
+	 *
+	 * @return bool
+	 */
+	public function safeUp()
+	{
+        // move current token table to old
+
+        if (craft()->db->tableExists('oauth_tokens'))
+        {
+            $this->renameTable('oauth_tokens', 'oauth_old_tokens');
+        }
+
+        // create new token table
+
+        if (!craft()->db->tableExists('oauth_tokens', true))
+        {
+            $this->createTable('oauth_tokens', array(
+                'providerHandle' => array('required' => true),
+                'pluginHandle'   => array('required' => true),
+                'encodedToken'   => array('column' => 'text'),
+            ), null, true);
+        }
+
+		return true;
+	}
+}
