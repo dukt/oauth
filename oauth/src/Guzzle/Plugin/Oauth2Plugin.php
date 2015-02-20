d8988d93a26cf74e9f3f5e8c1a5b9301dd035243
Benjamin David <ben@dukt.net>
2015-02-20 01:48:43 +0100

    Added OAuth 2.0 Guzzle plugin

---
+++ oauth/src/Guzzle/Plugin/Oauth2Plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Dukt\OAuth\Guzzle\Plugin;
+
+use Guzzle\Common\Event;
+use Guzzle\Http\Exception\BadResponseException;
+use Guzzle\Http\Message\RequestInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * OAuth2 plugin
+ * @link http://tools.ietf.org/html/rfc6749
+ */
+class Oauth2Plugin implements EventSubscriberInterface
+{
+    private $config;
+
+    public function __construct($config)
+    {
+        $this->config = $config;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'request.before_send' => 'onRequestBeforeSend'
+        );
+    }
+
+    public function onRequestBeforeSend(Event $event)
+    {
+        $request = $event['request'];
+        $accessToken = $this->config['access_token'];
+
+        switch($this->config['authorization_method'])
+        {
+            case 'oauth2_access_token':
+            $request->getQuery()->set('oauth2_access_token', $accessToken);
+            break;
+
+            default:
+            $request->getQuery()->set('access_token', $accessToken);
+        }
+    }
+
+
+}
