7c92273d295a30694ff6e503629de3cf6a6e5100
Benjamin David <ben@dukt.net>
2015-03-11 13:49:58 +0100

    Removed oauth_old_tokens table

---
+++ oauth/migrations/m150311_000001_oauth_remove_old_tokens.php
@@ -0,0 +1,20 @@
+<?php
+namespace Craft;
+
+/**
+ * The class name is the UTC timestamp in the format of mYYMMDD_HHMMSS_pluginHandle_migrationName
+ */
+class m150311_000001_oauth_remove_old_tokens extends BaseMigration
+{
+    /**
+     * Any migration code in here is wrapped inside of a transaction.
+     *
+     * @return bool
+     */
+    public function safeUp()
+    {
+        $this->dropTableIfExists('oauth_old_tokens');
+
+        return true;
+    }
+}
