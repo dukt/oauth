7d1b601c548696c51d0ef901b297174ce864cce4
Benjamin David <ben@dukt.net>
2015-05-06 15:15:06 +0200

    Clean up Provider

--- oauth/base/Provider.php
+++ oauth/base/Provider.php
@@ -27,7 +27,7 @@ abstract class Provider {
     public $storage = null;
     public $token = null;
     public $providerInfos = null;
-    protected $_provider;
+    protected $provider;
     protected $service = null;
     protected $scopes = array();
     protected $httpBuildEncType = 1;
@@ -35,54 +35,29 @@ abstract class Provider {
     // Public Methods
     // =========================================================================
 
-    public function getUserDetails()
+    public function getAccount()
     {
         $token = OauthHelper::getRealToken($this->token);
 
-        return $this->_provider->getUserDetails($token);
+        return $this->getProvider()->getUserDetails($token);
     }
 
     public function getAuthorizationUrl($options = array())
     {
-        $this->_provider->state = isset($options['state']) ? $options['state'] : md5(uniqid(rand(), true));
+        $this->getProvider()->state = isset($options['state']) ? $options['state'] : md5(uniqid(rand(), true));
 
         //options as params
         $params = $options;
 
         // set default
-        $params['client_id'] = $this->_provider->clientId;
-        $params['redirect_uri'] = $this->_provider->redirectUri;
-        $params['state'] = $this->_provider->state;
-        $params['scope'] = is_array($this->_provider->scopes) ? implode($this->_provider->scopeSeparator, $this->_provider->scopes) : $this->_provider->scopes;
+        $params['client_id'] = $this->getProvider()->clientId;
+        $params['redirect_uri'] = $this->getProvider()->redirectUri;
+        $params['state'] = $this->getProvider()->state;
+        $params['scope'] = is_array($this->getProvider()->scopes) ? implode($this->getProvider()->scopeSeparator, $this->getProvider()->scopes) : $this->getProvider()->scopes;
         $params['response_type'] = isset($options['response_type']) ? $options['response_type'] : 'code';
         $params['approval_prompt'] = isset($options['approval_prompt']) ? $options['approval_prompt'] : 'auto';
 
-        return $this->_provider->urlAuthorize().'?'.$this->httpBuildQuery($params, '', '&');
-    }
-
-    /**
-     * Build HTTP the HTTP query, handling PHP version control options
-     *
-     * @param  array        $params
-     * @param  integer      $numeric_prefix
-     * @param  string       $arg_separator
-     * @param  null|integer $enc_type
-     *
-     * @return string
-     * @codeCoverageIgnoreStart
-     */
-    protected function httpBuildQuery($params, $numeric_prefix = 0, $arg_separator = '&', $enc_type = null)
-    {
-        if (version_compare(PHP_VERSION, '5.4.0', '>=') && !defined('HHVM_VERSION')) {
-            if ($enc_type === null) {
-                $enc_type = $this->httpBuildEncType;
-            }
-            $url = http_build_query($params, $numeric_prefix, $arg_separator, $enc_type);
-        } else {
-            $url = http_build_query($params, $numeric_prefix, $arg_separator);
-        }
-
-        return $url;
+        return $this->getProvider()->urlAuthorize().'?'.$this->httpBuildQuery($params, '', '&');
     }
 
     public function getRedirectUri()
@@ -111,24 +86,19 @@ abstract class Provider {
         return $class;
     }
 
-    public function getTokens()
-    {
-        return \Craft\craft()->oauth->getTokensByProvider($this->getHandle());
-    }
-
-    public function initService()
-    {
-        $this->getProvider();
-    }
+    // public function getTokens()
+    // {
+    //     return \Craft\craft()->oauth->getTokensByProvider($this->getHandle());
+    // }
 
     public function getProvider()
     {
-        if (!isset($this->_provider))
+        if (!isset($this->provider))
         {
-            $this->_provider = $this->createProvider();
+            $this->provider = $this->createProvider();
         }
 
-        return $this->_provider;
+        return $this->provider;
     }
 
     public function getAuthorizationMethod()
@@ -138,11 +108,7 @@ abstract class Provider {
 
     public function setInfos(Oauth_ProviderInfosModel $provider)
     {
-        // set provider
         $this->providerInfos = $provider;
-
-        // re-initialize service with new scope
-        $this->initService();
     }
 
     public function getInfos()
@@ -152,27 +118,12 @@ abstract class Provider {
 
     public function setScopes(array $scopes)
     {
-        $this->_provider->scopes = $scopes;
-    }
-
-    public function getScopes()
-    {
-        return $this->scopes;
-    }
-
-    public function getParams()
-    {
-        return array();
+        $this->getProvider()->scopes = $scopes;
     }
 
     public function setToken(Oauth_TokenModel $token)
     {
         $this->token = $token;
-    }
-
-    public function getToken()
-    {
-        return $this->token;
     }
 
     /**
@@ -229,14 +180,32 @@ abstract class Provider {
         }
     }
 
+
+    // Protected Methods
+    // =========================================================================
+
     /**
-     * Get Account (alias)
+     * Build HTTP the HTTP query, handling PHP version control options
      *
-     * @deprecated Deprecated in 1.0.
-     * @return array
+     * @param  array        $params
+     * @param  integer      $numeric_prefix
+     * @param  string       $arg_separator
+     * @param  null|integer $enc_type
+     *
+     * @return string
+     * @codeCoverageIgnoreStart
      */
-    public function getAccount()
+    protected function httpBuildQuery($params, $numeric_prefix = 0, $arg_separator = '&', $enc_type = null)
     {
-        return $this->getUserDetails();
+        if (version_compare(PHP_VERSION, '5.4.0', '>=') && !defined('HHVM_VERSION')) {
+            if ($enc_type === null) {
+                $enc_type = $this->httpBuildEncType;
+            }
+            $url = http_build_query($params, $numeric_prefix, $arg_separator, $enc_type);
+        } else {
+            $url = http_build_query($params, $numeric_prefix, $arg_separator);
+        }
+
+        return $url;
     }
 }
