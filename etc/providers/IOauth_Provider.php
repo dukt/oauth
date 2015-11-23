74a764e311a589a848ce88cbf7a787db829fa53b
Benjamin David <ben@dukt.net>
2015-11-23 18:31:04 +0100

    Fixed comments

--- etc/providers/IOauth_Provider.php
+++ etc/providers/IOauth_Provider.php
@@ -1,16 +1,6 @@
 <?php
 namespace Craft;
 
-/**
- * Interface IPlugin
- *
- * @author    Pixel & Tonic, Inc. <support@pixelandtonic.com>
- * @copyright Copyright (c) 2014, Pixel & Tonic, Inc.
- * @license   http://buildwithcraft.com/license Craft License Agreement
- * @see       http://buildwithcraft.com
- * @package   craft.app.etc.plugins
- * @since     2.1
- */
 interface IOauth_Provider
 {
     // Public Methods
